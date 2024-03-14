1ce2bdd1ddd57ed88b9e6d44b168bbfaab71a1c1
radinmov <radinmovahedi1@gmail.com>
2024-03-14 20:49:30 +0330

    1.0.0

---
+++ src/pages/Home/index.js
@@ -0,0 +1,8 @@
+import useTitle from "../../componets/Hook/useTitle"
+
+export default function Home() {
+    const title = useTitle("Home(Dashboard)")
+    return (
+         <div>hello</div>
+    )        
+}
---
+++ src/router.js
@@ -0,0 +1,15 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+
+
+const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+
+]);
+
+export default function Router() {
+  return <RouterProvider router={routes} />;
+}
