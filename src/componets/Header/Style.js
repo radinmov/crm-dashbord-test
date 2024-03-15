1ac5066e7b2edbd7cced6eef6669cea867417a1f
radinmov <radinmovahedi1@gmail.com>
2024-03-15 13:09:03 +0330

    1.0.3

--- src/componets/Header/Style.js
+++ src/componets/Header/Style.js
@@ -1,7 +1,5 @@
 import styled from "styled-components";
 
 export const Style = styled.div`
-@tailwind base;
-@tailwind components;
-@tailwind utilities;    
+padding: 8px;
 `;
--- src/componets/Header/index.js
+++ src/componets/Header/index.js
@@ -1,44 +1,47 @@
 import { Link } from "react-router-dom";
+import { Style } from "./Style";
 
 
 export default function Header() {
     return (
-        <div className="wrapper-start">
-            <div className="wrapper-inner">
-                <img className="w-24 rounded-full" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgRG0dQYL8dorZDNiAS_zNS6TuOLvT_G3Xkc7DIpHtkQ&s" />
-                <div className="buttons">
-                    <Link className="flex gap-5 text-center">
-                        <img className="w-8" src="icons/png/dashboard.png" />
-                        <p className="text-black capitalize text-base">Dashboard</p>
-                    </Link>
-                    <Link className="flex gap-5 text-center">
-                        <img className="w-8 " src="icons/png/projects.png" />
-                        <p className="text-black capitalize text-base">Projects</p>
-                    </Link>
-                    <Link className="flex gap-5 text-center">
-                        <img className="w-8 " src="icons/png/bookmark.png" />
-                        <p className="text-black capitalize text-base">Calender</p>
-                    </Link>
-                    <Link className="flex gap-5 text-center">
-                        <img className="w-8 " src="icons/png/vacation.png" />
-                        <p className="text-black capitalize text-base">vacation</p>
-                    </Link>
-                    <Link className="flex gap-5 text-center">
-                        <img className="w-8 " src="icons/png/messenger.png" />
-                        <p className="text-black capitalize text-base">maeseger</p>
-                    </Link>
-                </div>
-                <div className="bottom-wrapper">
-                    <Link className="flex">
-                        <img className="w-8 " src="icons/png/messenger.png" />
-                        <p >Support</p>
-                    </Link>
-                    <Link className="flex">
-                        <img className="w-8 " src="icons/png/LogOut.png" />
-                        <p className="text-black">LogOut</p>
-                    </Link>
+        <Style >
+            <div className="wrapper-start w-48 h-dvh bg-white rounded-xl ">
+                <div className="wrapper-inner">
+                    <img className="w-24 rounded-full" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTgRG0dQYL8dorZDNiAS_zNS6TuOLvT_G3Xkc7DIpHtkQ&s" />
+                    <div className="buttons pt-14 p-3">
+                        <Link className="flex gap-5 text-center pt-5">
+                            <img className="w-8" src="icons/png/dashboard.png" />
+                            <p className="text-black capitalize text-lg">Dashboard</p>
+                        </Link>
+                        <Link className="flex gap-5 text-center pt-5">
+                            <img className="w-8 " src="icons/png/projects.png" />
+                            <p className="text-black capitalize text-lg">Projects</p>
+                        </Link>
+                        <Link className="flex gap-5 text-center pt-5">
+                            <img className="w-8 " src="icons/png/bookmark.png" />
+                            <p className="text-black capitalize text-lg">Calender</p>
+                        </Link>
+                        <Link className="flex gap-5 text-center pt-5">
+                            <img className="w-8 " src="icons/png/vacation.png" />
+                            <p className="text-black capitalize text-lg">vacation</p>
+                        </Link>
+                        <Link className="flex gap-5 text-center pt-5">
+                            <img className="w-8 " src="icons/png/messenger.png" />
+                            <p className="text-black capitalize text-lg">maeseger</p>
+                        </Link>
+                    </div>
+                    <div className="bottom-wrapper pt-48">
+                        <Link className="flex items-center gap-5 text-center  w-44 h-14 bg-blue-500  rounded-xl">
+                            <img className="w-8 " src="icons/png/messenger.png" />
+                            <p >Support</p>
+                        </Link>
+                        <Link className="flex  gap-2 text-center pt-5">
+                            <img className="w-8 " src="icons/png/LogOut.png" />
+                            <p className="text-black">LogOut</p>
+                        </Link>
+                    </div>
                 </div>
             </div>
-        </div>
+        </Style>
     )
 }
