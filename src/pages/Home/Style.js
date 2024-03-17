bfa0894f29a64a0f9ccb7ad7bdb0a6884fc37803
radinmov <radinmovahedi1@gmail.com>
2024-03-17 14:29:00 +0330

    1.0.5

--- src/pages/Home/Style.js
+++ src/pages/Home/Style.js
@@ -1,7 +1,8 @@
 import styled from "styled-components";
 
 export const Style = styled.div`
+    width:100%;
     height:100vh;
     background-color:#bfddf5   ;
-    
+
 `;  
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,29 +6,29 @@ export default function Home() {
     const title = useTitle("Home(Dashboard)")
     return (
     <Style  className="flex">
-         <Header />
-         <div className="wrapper_home_start">
+         <Header /> 
+         <div className="wrapper_home_start w-full">
             <div className="wrapper_home_start">
                 <div className="upper_start flex">
-                    <div className="int flex">
-                    <img alt="search_icon" src="icons/png/serach.png" />
-                    <input type="text" />
+                    <div className="int bg-white w-72 h-10 flex items-center rounded-md">
+                    <img className="w-8 h-5 m-3" alt="search_icon" src="icons/png/serach.png" />
+                    <input className="bg-inherit" placeholder="search" type="text" />
                     </div>
-                    <div className="alert_icon">
-                        <img alt="alert_icon" src="icons/png/aler.png" />
+                    <div className="alert_icon w-10 h-10 bg-white rounded-md  items-center">
+                        <img alt="alert_icon"  className="p-1.5" src="icons/png/aler.png" />
                     </div>
-                    <div className="user_name">
-                        <img alt="person_avatar" src="icons/png/avatar.png" />
-                        <p>person name </p>
+                    <div className="user_name flex items-center bg-white w-44 h-10 rounded-md ">
+                        <img alt="person_avatar" className="h-10 w-10" src="icons/png/avatar.png" />
+                        <p className="text-black p-1.5">jake moloeikoe  </p>
                     </div>
                 </div>
                 <div className="welcome_person_part">
-                    <p>Welcome Back person</p>
+                    <p className="text-slate-600 text-lg p-5">Welcome Back , jake</p>
                 </div>
-                <div className="starting_dash_part flex">
-                    <p>Dashboard</p>
-                    <div className="claender flex ">
-                        <img alt="calender_icon" src="icons/png/calender.png" />
+                <div className="starting_dash_part flex justify-around">
+                    <p className="text-black text-3xl">Dashboard</p>     
+                    <div className="claender  w-60 h-10 flex items-center bg-sky-500 rounded-md">
+                        <img className="m-2" alt="calender_icon" src="icons/png/calender.png" />
                         <p>2023/12/23</p>
                         <span>-</span>
                         <p>2024/3/16</p>
